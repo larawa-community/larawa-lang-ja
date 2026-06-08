4929911cbbda97555815de4784529fbfe5e0b5b6
LaraWA9527 <https://github.com/larawa9527>
2026-06-09 02:36:14 +0900

    init commitx

---
+++ resources/lang/ja/auth.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'failed' => '入力された認証情報は LaraWA と一致しません。',
+    'throttle' => 'ログイン試行回数が多すぎます。:seconds 秒後にもう一度お試しください。',
+    'disabled' => 'このユーザーアカウントは無効です。',
+    'login' => [
+        'title' => 'ログイン',
+        'subtitle' => 'セルフホスト型 WhatsApp API ゲートウェイ',
+        'email' => 'メールアドレス',
+        'password' => 'パスワード',
+        'remember' => 'このブラウザーを記憶する',
+        'submit' => 'ログイン',
+        'passkey' => 'パスキーでサインイン',
+    ],
+    'two_factor' => [
+        'challenge_title' => '二要素認証チャレンジ',
+        'title' => '二要素認証',
+        'subtitle' => '認証コードまたはリカバリーコードを入力してください。',
+        'code' => '確認コード',
+        'verify' => '確認',
+        'expired' => 'このログインチャレンジは無効です。',
+        'invalid' => '二要素認証コードが無効です。',
+    ],
+];
